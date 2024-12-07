0dff9187d9136f0c70b0434b08b65f690f205f11
Robert336 <rmazza33@gmail.com>
2024-12-06 19:59:53 -0500

    day 3 part 2 complete!

--- day3.go
+++ day3.go
@@ -12,6 +12,9 @@ import (
 // day 3 fix the shop keeper's computer: ignore the invalid characters to complete the multiplication
 // search for all occurances "mul(#,#)" where # is any int
 
+// part 2: keep tracks of do() and don't()
+// pretty much just a semaphore or flag that tells us to process a mul() or not.
+
 func main() {
 	fmt.Println("Advent of Code: Day 3")
 
@@ -24,16 +27,35 @@ func main() {
 	scanner := bufio.NewScanner(file)
 
 	sum := 0
+	mul_enabled := true
 
 	// parse file
-	r, _ := regexp.Compile(`mul\(\d+,\d+\)`)
+	// looks for 'mul(number,number)' or 'do()' or 'don't()'
+	r, _ := regexp.Compile(`(mul\(\d+,\d+\))|(do\(\))|(don't\(\))`)
 	for scanner.Scan() {
 		current_line := scanner.Text()
 		// search current_line for valid "mul" statements
 		//compute the mul statements and sum the results
 		matches := r.FindAllString(current_line, -1)
-		for _, mul_statement := range matches {
-			sum += EvaluateMul(mul_statement)
+
+		// enabled/disable mul
+
+		// process matching statements
+		for _, statement := range matches {
+			fmt.Println("Currently on: ", statement)
+			switch statement {
+			case "do()":
+				// enable
+				mul_enabled = true
+			case "don't()":
+				// disable
+				mul_enabled = false
+			default:
+				if mul_enabled {
+					fmt.Println("Evaluating: ", statement)
+					sum += EvaluateMul(statement)
+				}
+			}
 		}
 	}
 	fmt.Println("Sum: ", sum)
